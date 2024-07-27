646f20f144b7cd484d4fe423a3ede54c51d4ddf2
alfredliwewe <liwewerobati@gmail.com>
2024-07-27 14:48:09 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require 'db.php';
+?><!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Free Cell</title>
+</head>
+<body>
+
+</body>
+</html>
